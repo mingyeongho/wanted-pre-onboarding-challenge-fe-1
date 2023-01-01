218ad0fa7b5d8e9a32a4a17c9bb4424528703712
민경호 <1736s@naver.com>
2023-01-01 13:19:28 +0900

    design: Home Design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Todo from "../components/Todo";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  const onClickLogoutBtn = () => {
+    localStorage.removeItem("usersToken");
+    history.go(0);
+  };
 
   useEffect(() => {
     const usersToken = localStorage.getItem("usersToken");
     !usersToken && navigate("/auth");
   }, []);
 
-  return <main></main>;
+  return (
+    <main style={{ flexDirection: "column", gap: 10 }}>
+      <button onClick={onClickLogoutBtn}>로그아웃</button>
+      <Todo />
+    </main>
+  );
 };
 
 export default Home;
