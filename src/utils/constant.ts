55945d1f76cd5c359f37c675b4c308c8538b2929
민경호 <1736s@naver.com>
2023-01-01 13:20:46 +0900

    docs: add POST_TODOS_URL

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -6,6 +6,7 @@ export const LOGOUT = "로그아웃";
 export const POST_LOGIN_URL = "/users/login";
 export const POST_SIGNUP_URL = "/users/create";
 export const GET_TODOS_URL = "/todos";
+export const POST_TODOS_URL = "/todos";
 
 export const EMAIL = "Email";
 export const PASSWORD = "Password";
