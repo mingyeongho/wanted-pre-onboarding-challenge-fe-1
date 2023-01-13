8f2978de3c64fa53dbc6322e2843e7f14bf286ac
민경호 <1736s@naver.com>
2023-01-13 22:33:07 +0900

    feat: Router 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import Router from "./router/Router";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
